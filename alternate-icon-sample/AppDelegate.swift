3b3438d2834a9eb1470b64ce57a87977bf906203
kiwi-26 <winter-wind.sh@hotmail.co.jp>
2017-09-16 00:55:44 +0900

    Initial Commit

--- alternate-icon-sample/AppDelegate.swift
+++ alternate-icon-sample/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window!.makeKeyAndVisible()
+
+        let viewController = ViewController(nibName: nil, bundle: nil) as ViewController
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window!.rootViewController = navigationController
         return true
     }
 
--- alternate-icon-sample/ViewController.swift
+++ alternate-icon-sample/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    private let iconNames = ["Icon-App-A", "Icon-App-B", "Icon-App-C"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        let image = UIImage(named: iconNames[indexPath.row])
+        cell.textLabel?.text = iconNames[indexPath.row]
+        cell.imageView?.image = image
+        cell.imageView?.layer.cornerRadius = (image?.size.width)!/2
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let identifier = iconNames[indexPath.row]
+        
+        let application = UIApplication.shared
+        if indexPath.row == 0 {
+            application.setAlternateIconName(nil, completionHandler: nil)
+        } else {
+            application.setAlternateIconName(identifier, completionHandler: nil)
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
-
