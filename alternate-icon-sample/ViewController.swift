735fe2d0137c6f3a4c32fb088876eda3bba6e4b4
kiwi-26 <winter-wind.sh@hotmail.co.jp>
2017-09-17 11:17:21 +0900

    Add spotlight sample remove cell, refactoring

--- alternate-icon-sample/ViewController.swift
+++ alternate-icon-sample/ViewController.swift
@@ -14,10 +14,13 @@ import MobileCoreServices
 class ViewController: UITableViewController {
     
     private let iconNames = ["Icon-App-A", "Icon-App-B", "Icon-App-C"]
-    private let otherCellTexts = [("5秒後に通知を設定", "設定後、アプリを閉じてください"), ("Spotlightにサンプルを登録", nil)]
+    private let notificationCellTexts = [("10秒後に通知を設定", "設定後、アプリを閉じてください")]
+    private let spotlightCellTexts = [("Spotlightにサンプルを登録", "Kisekae と検索してください"), ("Spotlightからサンプルを削除", nil)]
+    private let searchableIndexName = "me.koga.alternate-icon-sample-searchable-index"
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(style: .grouped)
+        self.title = "Kisekae Sample"
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -33,7 +36,7 @@ class ViewController: UITableViewController {
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 3
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -41,6 +44,8 @@ class ViewController: UITableViewController {
         case 0:
             return 3
         case 1:
+            return 1
+        case 2:
             return 2
         default:
             return 0
@@ -52,7 +57,9 @@ class ViewController: UITableViewController {
         case 0:
             return "アイコンを変える"
         case 1:
-            return "その他"
+            return "プッシュ通知"
+        case 2:
+            return "Spotlight"
         default:
             return ""
         }
@@ -66,12 +73,19 @@ class ViewController: UITableViewController {
             let image = UIImage(named: iconNames[indexPath.row])
             cell.textLabel?.text = iconNames[indexPath.row]
             cell.imageView?.image = image
+
         case 1:
-            cell.textLabel?.text = otherCellTexts[indexPath.row].0
-            cell.detailTextLabel?.text = otherCellTexts[indexPath.row].1
+            cell.textLabel?.text = notificationCellTexts[indexPath.row].0
+            cell.detailTextLabel?.text = notificationCellTexts[indexPath.row].1
+            
+        case 2:
+            cell.textLabel?.text = spotlightCellTexts[indexPath.row].0
+            cell.detailTextLabel?.text = spotlightCellTexts[indexPath.row].1
+            
         default:
             break
         }
+        
         return cell
     }
     
@@ -84,50 +98,33 @@ class ViewController: UITableViewController {
             } else {
                 UIApplication.shared.setAlternateIconName(identifier, completionHandler: nil)
             }
+
         case 1:
-            switch indexPath.row {
-            case 0:
-                let notificationCenter = UNUserNotificationCenter.current()
-                notificationCenter.getNotificationSettings { settings in
-                    let status = settings.authorizationStatus
-                    switch status {
-                    case .authorized:
-                        self.setNotification()
-                    case .denied:
-                        break
-                    case .notDetermined:
-                        notificationCenter.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
-                            if granted {
-                                self.setNotification()
-                            }
+            let notificationCenter = UNUserNotificationCenter.current()
+            notificationCenter.getNotificationSettings { settings in
+                let status = settings.authorizationStatus
+                switch status {
+                case .authorized:
+                    self.setNotification()
+                case .denied:
+                    break
+                case .notDetermined:
+                    notificationCenter.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+                        if granted {
+                            self.setNotification()
                         }
                     }
                 }
+            }
 
+        case 2:
+            switch indexPath.row {
+            case 0:
+                addSpotlightSampleData()
+                
             case 1:
-                let spotlightTitle = "Kisekae spotlight sample"
-                let spotlightDescription = "Kisekae アプリの検索結果サンプルです"
+                deleteSpotlightSampleData()
                 
-                let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeContent as String)
-                attributeSet.title = spotlightTitle
-                attributeSet.contentDescription = spotlightDescription
-                attributeSet.keywords = [spotlightTitle, spotlightDescription]
-                attributeSet.identifier = "1234"
-                attributeSet.languages = ["ja", "en"]
-                
-                let domainIdentifier = "me.koga.alternate-icon-sample"
-                let searchableItem = CSSearchableItem(uniqueIdentifier: attributeSet.identifier, domainIdentifier: domainIdentifier, attributeSet: attributeSet)
-                
-                let searchableIndex = CSSearchableIndex(name: "me.koga.alternate-icon-sample-searchable-index")
-                
-                searchableIndex.fetchLastClientState { (clientState, error) in
-                    searchableIndex.beginBatch()
-                    
-                    searchableIndex.indexSearchableItems([searchableItem], completionHandler: nil)
-                    
-                    let clientState = NSKeyedArchiver.archivedData(withRootObject: NSDate())
-                    searchableIndex.endBatch(withClientState: clientState, completionHandler: nil)
-                }
             default:
                 break
             }
@@ -145,10 +142,64 @@ class ViewController: UITableViewController {
         content.body = "Notification Sample"
         content.sound = UNNotificationSound.default()
 
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
         let request = UNNotificationRequest(identifier: "FiveSecond", content: content, trigger: trigger)
         
         let notificationCenter = UNUserNotificationCenter.current()
-        notificationCenter.add(request, withCompletionHandler: nil)
+        notificationCenter.add(request, withCompletionHandler: { error in
+            guard error == nil else {
+                return
+            }
+            
+            self.showAlert(title: "登録しました", message: "アプリを閉じてお待ちください")
+        })
+    }
+    
+    private func addSpotlightSampleData() {
+        let spotlightTitle = "Kisekae spotlight sample"
+        let spotlightDescription = "Kisekae アプリの検索結果サンプルです"
+        
+        let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeContent as String)
+        attributeSet.title = spotlightTitle
+        attributeSet.contentDescription = spotlightDescription
+        attributeSet.keywords = [spotlightTitle, spotlightDescription]
+        attributeSet.identifier = "1234"
+        attributeSet.languages = ["ja", "en"]
+        
+        let searchableItem = CSSearchableItem(uniqueIdentifier: attributeSet.identifier, domainIdentifier: "me.koga.alternate-icon-sample", attributeSet: attributeSet)
+        
+        let searchableIndex = CSSearchableIndex(name: searchableIndexName)
+        
+        searchableIndex.fetchLastClientState { (clientState, error) in
+            searchableIndex.beginBatch()
+            
+            searchableIndex.indexSearchableItems([searchableItem], completionHandler: nil)
+            
+            let clientState = NSKeyedArchiver.archivedData(withRootObject: NSDate())
+            searchableIndex.endBatch(withClientState: clientState, completionHandler: { error in
+                guard error == nil else {
+                    return
+                }
+
+                self.showAlert(title: "登録しました", message: "Kisekae と検索してみてください")
+            })
+        }
+    }
+    
+    private func deleteSpotlightSampleData() {
+        let searchableIndex = CSSearchableIndex(name: searchableIndexName)
+        searchableIndex.deleteAllSearchableItems(completionHandler: { _ in
+            self.showAlert(title: "削除しました", message: nil)
+
+        })
+    }
+    
+    private func showAlert(title: String, message: String?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { _ in
+            alertController.dismiss(animated: true, completion: nil)
+        })
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
     }
 }
